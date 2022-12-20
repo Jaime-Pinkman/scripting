3d286eade22d64595d84186f9bc15418b3bafd17
Jaime-Pinkman <marathon.our@gmail.com>
2022-12-21 01:21:35 +0300

    added comments

--- game15/15puzzle.sh
+++ game15/15puzzle.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
+# Define the array of puzzle pieces
 arr=("0" "1" "2" "3" "4" "5" "6" "7" "8" "9" "A" "B" "C" "D" "E" "F" "@")
+
+# Save a copy of the original puzzle for checking later
 chk=("${arr[@]}")
 
+# Define the allowed movements
 mov=("4" "-4" "1" "-1")
+
+# Initialize some variables
 buf=""
 now=16
 pre=0
 count=0
 
+# Function to shuffle the puzzle
 shuffle() {
-
-    for i in "${!arr[@]}"; do
-        if [[ "${arr[$i]}" = "@" ]]; then
-            echo ${i}
-        fi
-    done
     # Seed the random number generator
     RANDOM=$$$(date +%s)
 
     # Shuffle the array
     for ((i=1; i<=16; i++)); do
-        # Generate a random index between 0 and the number of elements in the array
+        # Generate a random index between 1 and 15
         j=$(($RANDOM %15 + 1))
         # Swap the element at index i with the element at index j
         temp=${arr[$i]}
@@ -30,17 +31,26 @@ shuffle() {
     done
 }
 
+# Function to check if the puzzle is in the original configuration
 check() {
+    # Initialize result to 0 (puzzle is solved)
     res=0
+    # Check if each element in the puzzle is in the same position as in the original configuration
     for (( k = 1; k <= 16; k++ )) {
+        # If any element is not in the same position, set result to 1 (puzzle is not solved)
         [ ${arr[k]} != ${chk[k]} ] && res=1
     }
+    # Return result
     echo $res
 }
 
+# Function to determine the number of spaces to move the empty puzzle piece (indicated by "@")
+# in order to reach the puzzle piece with the specified value
 navigate() {
+    # Initialize variables to store the indices of the empty puzzle piece and the target puzzle piece
     declare -i dest
     declare -i src
+    # Find the indices of the empty puzzle piece and the target puzzle piece
     for i in "${!arr[@]}"; do
         if [[ "${arr[$i]}" = "$1" ]]; then
             dest=${i}
@@ -50,23 +60,31 @@ navigate() {
         fi
     done
 
+    # Calculate the difference between the indices (number of spaces to move)
     diff=`expr $dest - $src`
 
+    # Return the difference
     echo $diff
 }
 
+# Main function to play the puzzle game
 main() {
+    # Shuffle the puzzle
     shuffle
+    # Find the initial position of the empty puzzle piece
     for i in "${!arr[@]}"; do
         if [[ "${arr[$i]}" = "@" ]]; then
             now=${i}
         fi
     done
+    # Initialize the move counter and a flag for invalid moves
     declare -i count=1
     declare -i flag=0
+    # Start an infinite loop
     while :
     do
-        #clear
+        # Print the current state of the puzzle
+        clear
         echo "Ход № " $count
         printf \\n
         for i in {1..16}
